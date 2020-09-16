38e0901745c4157a8e2f91647c2f0fa5079c696e
spezifisch <spezifisch@users.noreply.github.com>
2020-09-16 14:49:34 +0200

    duplicate comment

--- fetch_test.go
+++ fetch_test.go
@@ -1,5 +1,4 @@
 // Copyright (C) 2020 Pascal Below (spezifisch)
-// Copyright (C) 2020 Pascal Below (spezifisch)
 
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
